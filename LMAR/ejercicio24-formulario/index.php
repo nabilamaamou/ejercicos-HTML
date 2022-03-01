47f64765a334da5aaae01a8fc5110f2ac977de8c
nabilamaamou <91939153+nabilamaamou@users.noreply.github.com>
2022-03-01 09:07:37 +0100

    Add files via upload

---
+++ LMAR/ejercicio24-formulario/index.php
@@ -0,0 +1,25 @@
+<?php
+echo "File php funciona";
+// string 
+// integer int 
+// float / double
+// boolean 
+// null
+//arry 
+// object 
+$texto = "Nabila"; //string
+echo $texto;
+$number =12;//integer
+echo $number;
+$double_number = 100.2541;
+
+echo "<h1>$double_number</h1>"; //double //float
+$boolean ="true";
+$boolean2 ="false";
+$name = null;
+echo $name;
+$numbers = array(2,5,9,5);
+echo $numbers[3]//array
+
+
+?>
